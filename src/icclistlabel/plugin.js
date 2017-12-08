5625b693e3e0421e686a5d24ed6670e1edbf83bf
Przemek Wiejak <pwiejak@iccsafe.org>
2017-12-08 16:01:09 -0600

    prevent breaking list by inserting exceptions into it

--- src/icclistlabel/plugin.js
+++ src/icclistlabel/plugin.js
@@ -1,7 +1,34 @@
 /*global CKEDITOR*/
 import keycode from 'keycode'
+import {toggleWidgetState} from "../../../ckeditor-iccexception-plugin/src/common/common";
 
 ((() => {
+    let title = 'icclistlabel';
+    let block = [
+        'indent',
+        'outdent',
+        'iccnumberedlist',
+        'iccbulletedlist',
+        'standardexception',
+        'exceptionlist'
+    ];
+
+    let blockNestedExceptions = (editor, event, names) => {
+        let sender = event.sender;
+        let label = sender.editables.label.$;
+        let widgets = [];
+
+        names.forEach(function (name) {
+            let widget = editor.commands[name] !== undefined ? editor.commands[name] : null;
+
+            if (widget) {
+                widgets.push(widget);
+            }
+        });
+
+        toggleWidgetState(widgets, label);
+    };
+
     const replaceRangeWithClosestEditableRoot = (range, element) => {
         if (range.root.equals(element)) {
             return range
@@ -11,22 +38,23 @@ import keycode from 'keycode'
             newRange.moveToRange(range)
             return newRange
         }
-    }
+    };
 
-    const iccListLabel = {
+    CKEDITOR.plugins.add(title, {
         requires: 'widget',
-        icons: 'icclistlabel',
-        init({widgets}) {
-            widgets.add(
-                'icclistlabel', {
+        icons: title,
+
+        init(editor) {
+            editor.widgets.add(
+                title, {
                     button: 'Add a list label',
                     template: '<span class="label">1.</span>',
+                    draggable: false,
                     editables: {
                         label: {
                             selector: 'span.label'
                         }
                     },
-                    draggable: false,
 
                     /**
                      * @param {CKEDITOR.htmlParser.element} element
@@ -36,11 +64,21 @@ import keycode from 'keycode'
                         return element.name === 'span' &&
                             element.hasClass('label') &&
                             element.getAscendant('p') !== null &&
-                            element.getAscendant('p').getAscendant('li') !== null
+                            element.getAscendant('p').getAscendant('li') !== null;
                     },
 
-                    init() {
-                        this.editor.on(
+                    // function fires when processing imported widget's editable area
+                    data: (event) => {
+                        blockNestedExceptions(editor, event, block);
+                    },
+
+                    // function fires when processing imported widget's editable area
+                    edit: (event) => {
+                        blockNestedExceptions(editor, event, block);
+                    },
+
+                    init: () => {
+                        editor.on(
                             'key', ({data: {keyCode, domEvent}, cancel}) => {
                                 // Check if target element matches this widget's element.
                                 if (this.element.equals(domEvent.getTarget())) {
@@ -49,17 +87,17 @@ import keycode from 'keycode'
                                     if (keyCode === keycode('delete') || keyCode === keycode('backspace')) {
                                         const selection = this.editor.getSelection()
                                         /** @type {CKEDITOR.dom.range} range */
-                                        let range = selection.getRanges()[0]
+                                        let range = selection.getRanges()[0];
 
                                         if (!range || !range.collapsed) {
-                                            return
+                                            return;
                                         }
 
-                                        range = replaceRangeWithClosestEditableRoot(range, this.element)
+                                        range = replaceRangeWithClosestEditableRoot(range, this.element);
 
                                         // If backspace is going to remove the label element, cancel the event.
                                         if (range.checkStartOfBlock() && range.getPreviousNode().equals(this.element)) {
-                                            cancel()
+                                            cancel();
                                         }
                                     }
                                 }
@@ -69,7 +107,5 @@ import keycode from 'keycode'
                 }
             )
         }
-    }
-
-    CKEDITOR.plugins.add('icclistlabel', iccListLabel)
-}))()
+    })
+}))();
